072f4db9456aa5edf3bd0b6a504911d0392ea208
Thotsana <thotsana.mabotsa@umuzi.org>
2020-04-28 03:46:48 +0200

    changed void methods to String type

---
+++ src/test/java/com/joshua/springboot/service/UserServiceImplTest.java
@@ -0,0 +1,21 @@
+package com.joshua.springboot.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceImplTest {
+
+    @Test
+    void addUser() {
+
+    }
+
+    @Test
+    void removeUser() {
+    }
+
+    @Test
+    void getUser() {
+    }
+}
